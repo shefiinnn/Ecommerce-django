94d5093282233c2c04a8827c5bc800c6cdff3079
shefiinnn <shazzshefin@gmail.com>
2025-05-25 19:46:34 +0530

    updates

--- ecom/ecom/settings.py
+++ ecom/ecom/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'ecom.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['template'],
+        'DIRS': ["template"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
